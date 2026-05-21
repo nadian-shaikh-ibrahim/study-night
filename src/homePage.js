8348eb2f06dabc79163a51730479ecd417f548fd
nadian-shaikh-ibrahim <nadbj05@gmail.com>
2026-05-21 20:04:39 +0000

    fix:image loading with parcel url scheme ,add cypress config and simplify css selectors

--- src/homePage.js
+++ src/homePage.js
@@ -1,7 +1,7 @@
 //DO NOT CHANGE ANYTHING IN THIS FILE//
 // This file is responsible for loading the home page
 //Imports image for homepage
-import homePageImage from "../images/homePage.png";
+import homePageImage from "url:../images/homePage.png";
 //Helper functions
 import {
   createHeader,
